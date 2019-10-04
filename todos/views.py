394658f0b3bbf840a8573ea24960501f2aff7163
Sunny <sunnyyong2@gmail.com>
2019-10-04 11:12:17 +0900

    create 완성

--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from todos.models import Todo
 
 # Create your views here.
 def index(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos' : todos,
+    }
     return render(request, 'index.html')
 
 def new(request):
     return render(request, 'new.html')
 
 def create(request):
-    pass
+    author = request.POST.get('author')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    due_date = request.POST.get('due-date')
+
+    # todo = Todo()
+    # todo.title = title
+    # todo.save()
+
+    # todo = Todo(title=title, content=content)
+    # todo.save()
+
+    todo = Todo.objects.create(
+        author=author, 
+        title=title, 
+        content=content, 
+        due_date=due_date
+        )
+    
+    return redirect('/todos/')
